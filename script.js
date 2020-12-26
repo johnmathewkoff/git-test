ae1650f5aadec758d2733f79737e0199b207457c
johnmathewkoff <71914469+johnmathewkoff@users.noreply.github.com>
2020-12-26 12:30:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+'use strict';
+
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('How many films do you wathed?', '');
+
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
+            personalMovieDB.count = +prompt('How many films do you wathed?', '');
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('One of the last wathed films?', ''),
+                  b = +prompt('How much do you like it?', '');
+            
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectMyPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log('Common');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('Liker');
+        } else if (personalMovieDB.count >= 30) {
+            console.log('Kinomania');
+        } else {
+            console.log('error');
+        }
+    },
+    writeYourGenres: function() {
+        // for (let i = 1; i < 4; i++) {
+        //     let genre = prompt(`Your favourite genre numbre ${i}`);
+
+        //     if (genre === '' || genre === null) {
+        //         console.log('Incorrect');
+        //         i--;
+        //     } else {
+        //         personalMovieDB.genres[i - 1] = genre;
+        //     }
+        // }
+
+        for (let i = 1; i < 2; i++) {
+            let genres = prompt('Your favourite films');
+
+            if (genres === '' || genres === null) {
+                console.log('inscorresct');
+                i--;
+            } else {
+                personalMovieDB.genres = genres.split(', ');
+            }
+        }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Favourite genre number ${i + 1}: is ${item}`);
+        });
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat === false) {
+            personalMovieDB.privat = true;
+        } else if (personalMovieDB.privat === true){
+            personalMovieDB.privat = false;
+        }
+    },
+    showMyDB: function() {
+        if (personalMovieDB.privat === false){
+            console.log(personalMovieDB);
+        }
+    }
+};
+
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectMyPersonalLevel();
+personalMovieDB.showMyDB(personalMovieDB);
+personalMovieDB.writeYourGenres();
+personalMovieDB.toggleVisibleMyDB();
+
+// let i = 0;
+
+// while (i < 2) {
+    
+//     const a = prompt('One of the last wathed films?', ''),
+//           b = +prompt('How much do you like it?', '');
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+
+//     i++;
+// }
+
+// let i = 0;
+
+// do {
+//     const a = prompt('One of the last wathed films?', ''),
+//           b = +prompt('How much do you like it?', '');
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//         i++;
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+
+// } while (i < 2);
